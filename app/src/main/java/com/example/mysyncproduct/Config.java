54b00cc91ff2201b839f7e73851c68e1888d163b
Sachintha <hmsachinthaanujaya@gmail.com>
2021-01-08 13:02:15 +0530

    aquarium mobile app

--- app/src/main/java/com/example/mysyncproduct/Config.java
+++ app/src/main/java/com/example/mysyncproduct/Config.java
@@ -1,5 +1,5 @@
 package com.example.mysyncproduct;
-//email password
+//email
 public class Config {
     public static final String EMAIL ="devinda.inspiredlk@gmail.com"; //your-gmail-username
     public static final String PASSWORD ="Devinda1997#"; //your-gmail-password
